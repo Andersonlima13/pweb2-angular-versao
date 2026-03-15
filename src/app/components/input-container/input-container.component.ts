3607153a501f94ec954a8a85eac00ce962d78dfc
Andersonlima13 <anderson.lima18pb@gmail.com>
2026-03-14 23:25:13 -0300

    tela de registro concluida

--- src/app/components/input-container/input-container.component.ts
+++ src/app/components/input-container/input-container.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ReactiveFormsModule, FormGroup } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-input-container',
@@ -10,10 +10,5 @@ import { ReactiveFormsModule, FormGroup } from '@angular/forms';
   styleUrl: './input-container.component.css'
 })
 export class InputContainerComponent {
-  @Input({ required: true }) form!: FormGroup;
-  @Output() submitted = new EventEmitter<void>();
-
-  onSubmit() {
-    this.submitted.emit();
-  }
+  
 }
